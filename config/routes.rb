da6255ef486ff9cbd3da6021e6aca6e200a89a26
lnsjhnssn <144560735+lnsjhnssn@users.noreply.github.com>
2024-08-21 10:33:14 +0200

    added deletebutton

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :api do
+
     resources :users, only: [:create]
 
     get    'tasks'                    => 'tasks#index'
